02f536d8da009615517de81a0091e0152a4b69f2
Philip Mørch <mrphil2105@gmail.com>
2022-04-03 13:12:59 +0200

    Add database context and user account classes

---
+++ CafeAnalog/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeAnalog.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
---
+++ CafeAnalog/Data/AppUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace CafeAnalog.Data;
+
+public class AppUser : IdentityUser
+{
+}
