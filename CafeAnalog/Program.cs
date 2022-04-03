e06056ee30b3674b3fc8af3c58e89b8bc5598444
Philip Mørch <mrphil2105@gmail.com>
2022-04-03 13:14:36 +0200

    Configure MVC, development settings, database and identity

--- CafeAnalog/Program.cs
+++ CafeAnalog/Program.cs
@@ -1,6 +1,48 @@
+using CafeAnalog.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+services.AddDbContext<AppDbContext>(o => o.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+services.AddDatabaseDeveloperPageExceptionFilter();
+
+services.AddDefaultIdentity<AppUser>(o =>
+    {
+        o.Password.RequiredLength = 8;
+        o.Password.RequireNonAlphanumeric = false;
+    })
+    .AddEntityFrameworkStores<AppDbContext>();
+
+var mvcBuilder = services.AddControllersWithViews();
+
+if (builder.Environment.IsDevelopment())
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
