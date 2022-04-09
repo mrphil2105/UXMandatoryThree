1774048918e5f57082024d4884692cb251d0da2a
Philip Mørch <mrphil2105@gmail.com>
2022-04-09 14:20:55 +0200

    Add first name and last name to users

--- CafeAnalog/Data/AppDbContext.cs
+++ CafeAnalog/Data/AppDbContext.cs
@@ -8,4 +8,21 @@ public class AppDbContext : IdentityDbContext<AppUser>
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // User
+
+        builder.Entity<AppUser>()
+            .Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Entity<AppUser>()
+            .Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
 }
--- CafeAnalog/Data/AppUser.cs
+++ CafeAnalog/Data/AppUser.cs
@@ -4,4 +4,7 @@ namespace CafeAnalog.Data;
 
 public class AppUser : IdentityUser
 {
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
 }
