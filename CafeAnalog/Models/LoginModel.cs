0275691defd2ba13aed283d570f8d0a2eadec653
Philip Mørch <mrphil2105@gmail.com>
2022-04-10 19:38:37 +0200

    Remove form labels in favor of placeholders

--- CafeAnalog/Models/LoginModel.cs
+++ CafeAnalog/Models/LoginModel.cs
@@ -5,10 +5,12 @@ namespace CafeAnalog.Models;
 public class LoginModel
 {
     [Required]
+    [Display(Prompt = "Email")]
     public string? Email { get; init; }
 
     [Required]
     [DataType(DataType.Password)]
+    [Display(Prompt = "Password")]
     public string? Password { get; init; }
 
     [Display(Name = "Keep me signed in")]
--- CafeAnalog/Models/RegisterModel.cs
+++ CafeAnalog/Models/RegisterModel.cs
@@ -6,28 +6,30 @@ public class RegisterModel
 {
     [Required]
     [MaxLength(100)]
-    [Display(Name = "First Name")]
+    [Display(Prompt = "First Name")]
     public string? FirstName { get; init; }
 
     [Required]
     [MaxLength(100)]
-    [Display(Name = "Last Name")]
+    [Display(Prompt = "Last Name")]
     public string? LastName { get; init; }
 
     [Required]
     [EmailAddress]
+    [Display(Prompt = "Email")]
     public string? Email { get; init; }
 
     [Phone]
-    [Display(Name = "Mobile Number")]
+    [Display(Prompt = "Mobile Number")]
     public string? MobileNumber { get; init; }
 
     [Required]
     [DataType(DataType.Password)]
+    [Display(Prompt = "Password")]
     public string? Password { get; init; }
 
     [DataType(DataType.Password)]
     [Compare(nameof(Password))]
-    [Display(Name = "Confirm Password")]
+    [Display(Prompt = "Confirm Password")]
     public string? PasswordConfirmation { get; init; }
 }
