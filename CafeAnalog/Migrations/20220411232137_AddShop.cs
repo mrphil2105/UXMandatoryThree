e6a81bec60d1dd5509a67307e0c21a057c901bb6
Philip Mørch <mrphil2105@gmail.com>
2022-04-12 01:22:28 +0200

    Add database migrations for shop

---
+++ CafeAnalog/Migrations/20220411232137_AddShop.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeAnalog.Migrations
+{
+    public partial class AddShop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShopCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShopItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShopItems_ShopCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "ShopCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopCategories_Name",
+                table: "ShopCategories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopItems_CategoryId",
+                table: "ShopItems",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopItems_Name",
+                table: "ShopItems",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShopItems");
+
+            migrationBuilder.DropTable(
+                name: "ShopCategories");
+        }
+    }
+}
