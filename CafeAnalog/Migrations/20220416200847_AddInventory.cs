70147b229b129f81714e46ff7cf8873fb7a468cc
Philip Mørch <mrphil2105@gmail.com>
2022-04-16 22:33:29 +0200

    Add database migrations for inventory tickets

---
+++ CafeAnalog/Migrations/20220416200847_AddInventory.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeAnalog.Migrations
+{
+    public partial class AddInventory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "InventoryTickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Count = table.Column<int>(type: "INTEGER", nullable: false),
+                    ItemId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryTickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InventoryTickets_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_InventoryTickets_ShopItems_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "ShopItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryTickets_ItemId",
+                table: "InventoryTickets",
+                column: "ItemId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryTickets_UserId",
+                table: "InventoryTickets",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InventoryTickets");
+
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "AspNetUsers");
+        }
+    }
+}
