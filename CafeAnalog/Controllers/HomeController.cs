a4ef4810d42193d01fc7e7f7153d6e575cd5e383
COHL7500 <cohl7500@gmail.com>
2022-04-25 23:33:57 +0200

    Set up design of Leaderboard

--- CafeAnalog/Controllers/HomeController.cs
+++ CafeAnalog/Controllers/HomeController.cs
@@ -6,4 +6,9 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    public IActionResult Leaderboard()
+    {
+        return View();
+    }
 }
---
+++ CafeAnalog/Models/LeaderboardModel.cs
@@ -0,0 +1,10 @@
+namespace CafeAnalog.Models;
+
+public class LeaderboardModel
+{
+    public string FirstName { init; get; }
+    
+    public string LastName { init; get; }
+    
+    public int Score { init; get; }
+}
