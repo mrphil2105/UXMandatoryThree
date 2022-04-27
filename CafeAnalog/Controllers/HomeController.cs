27c10a1d7ffb55ec2951bc2b6385ffe5bd46a68a
COHL7500 <cohl7500@gmail.com>
2022-04-27 17:22:03 +0200

    generate leaderboard from user database

--- CafeAnalog/Controllers/HomeController.cs
+++ CafeAnalog/Controllers/HomeController.cs
@@ -1,14 +1,33 @@
+using CafeAnalog.Data;
+using CafeAnalog.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace CafeAnalog.Controllers;
 
 public class HomeController : Controller
 {
+    
+    private readonly AppDbContext _dbContext;
+
+    public HomeController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Leaderboard()
+    public async Task<IActionResult> Leaderboard()
     {
-        return View();
+        var users = await _dbContext.Users.ToListAsync();
+
+        var rankings = users.Select(u =>
+                new RankingModel {Score = u.Score, FirstName = u.FirstName, LastName = u.LastName})
+            .OrderByDescending(r => r.Score)
+            .ToList();
+        
+        return View(rankings);
     }
 }
