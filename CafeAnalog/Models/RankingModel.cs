d4957c5298ae505553982560f4c68902f526306d
Philip Mørch <mrphil2105@gmail.com>
2022-05-03 15:07:24 +0200

    Clean up code

--- CafeAnalog/Models/RankingModel.cs
+++ CafeAnalog/Models/RankingModel.cs
@@ -3,8 +3,8 @@ namespace CafeAnalog.Models;
 public class RankingModel
 {
     public string? FirstName { init; get; }
-    
+
     public string? LastName { init; get; }
-    
+
     public int Score { init; get; }
 }
